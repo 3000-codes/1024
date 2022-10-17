01128dac199f520a269fded5d893cbdf905b2532
panjh <panjh@augurit.com>
2022-10-17 09:54:38 +0800

    lifecycle

--- my-components/src/components/jsx/funtion-component.tsx
+++ my-components/src/components/jsx/funtion-component.tsx
@@ -1,1 +1,11 @@
-export default (props:any, ctx:any) => <div>{props.message}</div>
+// import { onMounted } from "vue";
+export default (props: any, ctx: any) => {
+  // 函数组件无法使用生命周期
+  // onMounted(() => {
+  //   console.log('onMounted');
+
+  // })
+  // const emits = defineEmits(['send'])
+  const send = () => ctx.emit('send', 123)
+  return (<><div>{props.message}</div><button onClick={send}>send</button></>)
+}
