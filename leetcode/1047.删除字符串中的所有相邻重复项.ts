4f315cad9adc0d13f1e70e6dd44ea964ba5c98b1
panjh <53244269+3000-codes@users.noreply.github.com>
2022-11-17 09:19:36 +0800

    format

--- leetcode/1047.删除字符串中的所有相邻重复项.ts
+++ leetcode/1047.删除字符串中的所有相邻重复项.ts
@@ -2,19 +2,19 @@
  * @lc app=leetcode.cn id=1047 lang=typescript
  *
  * [1047] 删除字符串中的所有相邻重复项
+ * abbac
  */
 
 // @lc code=start
-function removeDuplicates(s: string): string {
-const stack:string[]=[]
-for(let v of s){
-  const prev=stack.pop()
-  if(prev!==v){
-    prev&& stack.push(prev)
-    stack.push(v)
+function removeDuplicates (s: string): string {
+  const stack:string[] = []
+  for (const v of s) {
+    const prev = stack.pop()
+    if (prev !== v) {
+      prev && stack.push(prev) // X a
+      stack.push(v) // a b
+    }
   }
-}
-return stack.join('')
+  return stack.join('')
 };
 // @lc code=end
-
--- leetcode/70.爬楼梯.ts
+++ leetcode/70.爬楼梯.ts
@@ -6,9 +6,16 @@
 
 // @lc code=start
 function climbStairs(n: number): number {
-if(n===1) return 1
-if(n===2) return 2
-return climbStairs(n-1)+climbStairs(n-2)
+// if(n===1) return 1
+// if(n===2) return 2
+// return climbStairs(n-1)+climbStairs(n-2)
+  const arr=[1,1,2]
+  if(!arr[n]) {
+    for(let i=3;i<=n;i++){
+    arr[i]=arr[i-1]+arr[i-2]
+    }
+  }
+  return arr[n]
 };
 // @lc code=end
 
