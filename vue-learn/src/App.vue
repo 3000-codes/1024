2509035e3e3d57c21dea51280e07efd8027f323f
panjh <53244269+3000-codes@users.noreply.github.com>
2022-12-07 20:01:31 +0800

    init mylodash

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import { Observable } from 'rxjs'
+const count = ref<number>(0)
+const test = ref<HTMLDivElement>()
+const increment = () => {
+  count.value++
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+const observer = {
+  next: (x: any) => console.log('Observer got a next value: ' + x),
+  error: (err: any) => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification')
+}
+onMounted(() => {
+  const observable$ = new Observable((subscriber: any) => {
+    subscriber.next(1)
+    subscriber.next(2)
+    subscriber.next(3)
+    subscriber.complete()
+  })
+  observable$.subscribe(observer)
+})
 </script>
 
 <template>
