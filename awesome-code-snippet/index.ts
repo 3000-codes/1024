db1a64021a67b51f6d48fdf32f00ec40db21f441
panjh <53244269+3000-codes@users.noreply.github.com>
2023-01-11 17:06:29 +0800

    测试await

--- awesome-code-snippet/index.ts
+++ awesome-code-snippet/index.ts
@@ -51,3 +51,29 @@
   const v4 = Symbol("foo");
   console.log(Object.assign({}, v1, null, v2, undefined, v3, v4)); // => {0: 'a', 1: 'b', 2: 'c'}
 })(globalThis);
+(function () {
+  const delay = (ms: number) =>{console.log('delaying...'); return new Promise((resolve) => setTimeout(resolve, ms));}
+  const fn1 = async () => {
+    const start=performance.now();
+    await delay(1000);
+    await delay(500);
+    return performance.now()-start;
+  };
+  const fn2 = async () => {
+    const start=performance.now();
+    const p1= delay(1000);
+    const p2= delay(500);
+    await p1;
+    await p2;
+    return performance.now()-start;
+  }
+  Promise.all([fn1(),fn2()]).then(([r1,r2])=>console.log(r1-r2)); 
+  // Normally, fn1() is faster than fn2() about 500ms.
+  // But in edge, the time difference is nearly 1000ms.
+})();
+(function () {
+  // js不存在深克隆
+  const a:any={}
+  a.a=a;
+  const b=JSON.parse(JSON.stringify(a));
+})();
